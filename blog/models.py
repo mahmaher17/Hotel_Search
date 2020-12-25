0ff7ab2cf439b71b86b6749f8f3ffff0343f2390
mahmoud <mahmoudelshenawy619@gmail.com>
2020-12-25 01:12:13 -0800

    design my first model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Create your models here.# Create database here dah m3ana el models 
+
+
+class post (models.Model) : # db Table created 
+    title = models.CharField ( max_length = 50 ) # Column 
+    content = models.TextField ( max_length = 2000 ) # another column
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__ (self) : # hena 3lshan a5leha yerga3ly be title fe panel 
+        return self.title
