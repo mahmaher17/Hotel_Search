f87692407a2f4b83ebe57905bc864a684049f826
mahmoud <mahmoudelshenawy619@gmail.com>
2020-12-28 03:26:18 -0800

    add more items in my post like timezone

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,8 @@ class post (models.Model) : # db Table created
     title = models.CharField ( max_length = 50 ) # Column 
     content = models.TextField ( max_length = 2000 ) # another column
     created_at = models.DateTimeField(default=timezone.now)
+    active = models.BooleanField(default=False)
+    author_mail= models.EmailField(default='')
 
     def __str__ (self) : # hena 3lshan a5leha yerga3ly be title fe panel 
         return self.title
